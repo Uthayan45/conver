3b5ec3453ee13517772e35b7f1947eb7bc1d8f21
Uthayan <144508677+Uthayan45@users.noreply.github.com>
2025-11-05 12:15:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173", // Vite default port
+    origin: "https://conver-amber.vercel.app/", // Vite default port
     methods: ["GET", "POST"]
   }
 });
